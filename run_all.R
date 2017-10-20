e8083faf3792de5a88f184278e5e224d5624bd8b
Brandon <brandon.foster5150@gmail.com>
2017-10-20 16:12:18 -0400

    commit
    starting analyses

--- run_all.R
+++ run_all.R
@@ -1,7 +1,16 @@
 # compiles all the reports in the .rmds/ directory
-source("functions/make_reports.R")
-
+#source("functions/make_reports.R")
+pacman::p_load(rmarkdown) 
 # compile individual reports
-#report("rmds/01_Import.Rmd")
-#report("rmds/02_Tidy.Rmd")
-#report("rmds/03_Analyses.Rmd")
+render("rmds/01_Import.Rmd", "all", output_dir = "reports")
+render("rmds/02_Tidy.Rmd", "all", output_dir = "reports")
+render("rmds/04_Analyses.Rmd", "all", output_dir = "reports")
+render("", "all", output_dir = "reports")
+
+# render(input, output_format = NULL, output_file = NULL, output_dir = NULL,
+#        output_options = NULL, intermediates_dir = NULL,
+#        knit_root_dir = NULL,
+#        runtime = c("auto", "static", "shiny", "shiny_prerendered"),
+#        clean = TRUE, params = NULL, knit_meta = NULL, envir = parent.frame(),
+#        run_pandoc = TRUE, quiet = FALSE, encoding = getOption("encoding"))
+render("rmds/01_Import.Rmd", "all", output_dir = "reports")
