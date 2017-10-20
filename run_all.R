98d78f2ffc7f38bb4dada812c5a736f9e24ce9e5
Brandon <brandon.foster5150@gmail.com>
2017-10-20 14:37:22 -0400

    commit

---
+++ run_all.R
@@ -0,0 +1,7 @@
+# compiles all the reports in the .rmds/ directory
+source("functions/make_reports.R")
+
+# compile individual reports
+#report("rmds/01_Import.Rmd")
+#report("rmds/02_Tidy.Rmd")
+#report("rmds/03_Analyses.Rmd")
