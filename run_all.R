6a458c0d79d8b23d8871aa780e23886a3efd4d88
Brandon <brandon.foster5150@gmail.com>
2017-10-21 15:21:36 -0400

    commit
    commit

--- run_all.R
+++ run_all.R
@@ -1,10 +1,13 @@
 # compiles all the reports in the .rmds/ directory
 #source("functions/make_reports.R")
+if (!("pacman" %in% installed.packages())){
+  install.packages("pacman")
+}
 pacman::p_load(rmarkdown) 
 # compile individual reports
 render("rmds/01_Import.Rmd", "all", output_dir = "reports")
 render("rmds/02_Tidy.Rmd", "all", output_dir = "reports")
-render("rmds/04_Analyses.Rmd", "all", output_dir = "reports")
+render("rmds/03_Analyses.Rmd", output_dir = "reports")
 render("", "all", output_dir = "reports")
 
 # render(input, output_format = NULL, output_file = NULL, output_dir = NULL,
