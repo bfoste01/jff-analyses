8128b6eb8bc05ca2b4d56022280ea53e7ef8a21f
Brandon <brandon.foster5150@gmail.com>
2017-11-01 12:04:50 -0400

    commit
    commit

--- functions/functions.r
+++ functions/functions.r
@@ -18,3 +18,26 @@ hc <- hclust(dd)
 cormat <-cormat[hc$order, hc$order]
 }
 
+
+# Function to tidy lavaan output
+tidy.lavaan <- function(x, 
+                        conf.int = TRUE,
+                        conf.level = 0.95,
+                        standardized=FALSE,
+                        ...){
+    tidyframe <- parameterEstimates(x, 
+                       ci=conf.int, 
+                       level=conf.level,
+                       standardized=standardized) %>% 
+        as_data_frame() %>% 
+        tibble::rownames_to_column() %>% 
+        mutate(term=paste(lhs, op, rhs)) %>% 
+        rename(estimate=est, 
+               std.error = se,
+               p.value=pvalue,
+               statistic = z,
+               conf.low=ci.lower,
+               conf.hi=ci.upper) %>% 
+        select(term, op, everything(), -rowname, -lhs, -rhs)
+    return(tidyframe)
+}
