8aabe7e615d5a6b012a396226bab8ffef3da1804
Brandon <brandon.foster5150@gmail.com>
2017-11-01 12:24:44 -0400

    commit
    commit

--- run_all.R
+++ run_all.R
@@ -8,8 +8,9 @@ pacman::p_load(rmarkdown)
 render("rmds/01_Import.Rmd", "all", output_dir = "reports")
 render("rmds/02_Tidy.Rmd", "all", output_dir = "reports")
 render("rmds/03_Analyses.Rmd", output_dir = "reports")
-render("rmds/04_Analyses.Rmd",  output_dir = "../reports/", html_document(toc = TRUE, toc_depth = 3, toc_float = TRUE, theme = "superhero",
-                                             code_folding = c("hide")))
+render("rmds/04_Analyses.Rmd",  output_dir = "reports", 
+       html_document(toc = TRUE, toc_depth = 3, toc_float = TRUE, theme = "sandstone",
+                     code_folding = c("hide")))
 
 # valid themes: cerulean, cosmo, cyborgh, darkly, flatly, journal, lumen, paper, readable, sandstone, simplex, slate, solar, spacelan, superhero, united, yeti
 # render(input, output_format = NULL, output_file = NULL, output_dir = NULL,
